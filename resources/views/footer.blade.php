9f88c6d5db6b167c0c5634942d501e3e9108caf7
EdinsonSV <edinsonsanvil@gmail.com>
2023-08-15 10:03:27 -0500

    Primer Avance

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="w-[calc(100%-3.73rem)] ml-auto bottom-0">
+    <div class="2xl:container mx-auto">
+        <div class="h-16 border-t border-gray-300/40 dark:border-gray-700"><h3 class="dark:text-gray-300">ETodos los derechos reservados. Diseñado y desarrollado por Industrias Balinsa.</h3></div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('aside')
+<main class="w-[calc(100%-3.73rem)] ml-auto h-[calc(100%-72px)]">
+    <div class="2xl:container mx-auto">
+        <div class="h-16 border-b border-gray-300/40 dark:border-gray-700">Buenos Dias</div>
+        <div class="px-6 lg:px-12">
+            <div class="h-96 flex justify-center items-center rounded-2xl border border-dashed border-gray-300/40 dark:border-gray-700">
+                <span class="text-gray-500 dark:text-gray-200">Content</span>
+            </div>
+        </div>
+    </div>
+</main>
+@extends('footer')
