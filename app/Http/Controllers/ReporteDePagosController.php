d611d9a8a7bb5e0c1fb40d8f1ba56f3909055179
EdinsonSV <104914689+EdinsonSV@users.noreply.github.com>
2023-12-29 09:20:23 -0500

    Cambio 6 ReporteDePagosController.php

--- app/Http/Controllers/ReporteDePagosController.php
+++ app/Http/Controllers/ReporteDePagosController.php
@@ -206,10 +206,6 @@ class ReporteDePagosController extends Controller
     public function consulta_Descuentos($codigoCli, $fechaInicio, $fechaFin) {
         $consulta = DB::table('tb_descuentos')
             ->selectRaw('fechaRegistroDesc')
-            ->selectRaw('SUM(IF(especieDesc = 1 AND pesoDesc < 0.0, pesoDesc, 0)) AS totalPesoDescuentoCuartaEspeciePrimerEspecie')
-            ->selectRaw('SUM(IF(especieDesc = 2 AND pesoDesc < 0.0, pesoDesc, 0)) AS totalPesoDescuentoCuartaEspecieSegundaEspecie')
-            ->selectRaw('SUM(IF(especieDesc = 3 AND pesoDesc < 0.0, pesoDesc, 0)) AS totalPesoDescuentoCuartaEspecieTerceraEspecie')
-            ->selectRaw('SUM(IF(especieDesc = 4 AND pesoDesc < 0.0, pesoDesc, 0)) AS totalPesoDescuentoCuartaEspecieCuartaEspecie')
             ->selectRaw('SUM(pesoDesc) AS totalPesoDescuento')
             ->selectRaw('SUM(pesoDesc * precioDesc) AS totalVentaDescuento')
             ->where('codigoCli', $codigoCli)
